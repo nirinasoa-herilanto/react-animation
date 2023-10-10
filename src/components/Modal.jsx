b80b26660e7b47e7c6ce6dc851326a1cd84433a7
nirinasoa-herilanto <nirinasoa.herilanto@gmail.com>
2023-10-10 10:22:55 +0300

    feat: Adding Entry Animations

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,13 +1,25 @@
 import { createPortal } from 'react-dom';
+import { motion } from 'framer-motion';
 
 export default function Modal({ title, children, onClose }) {
   return createPortal(
     <>
       <div className="backdrop" onClick={onClose} />
-      <dialog open className="modal">
+      <motion.dialog
+        initial={{
+          y: 30,
+          opacity: 0,
+        }}
+        animate={{
+          y: 0,
+          opacity: 1,
+        }}
+        open
+        className="modal"
+      >
         <h2>{title}</h2>
         {children}
-      </dialog>
+      </motion.dialog>
     </>,
     document.getElementById('modal')
   );
